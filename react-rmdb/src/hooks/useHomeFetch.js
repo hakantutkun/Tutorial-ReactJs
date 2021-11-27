db09753e5ec7b7fa335af29f8ec98b02825adeca
Hakan Tutkun <hakantutkun@gmail.com>
2021-11-28 02:26:35 +0300

    HeroImage component created.

--- react-rmdb/src/hooks/useHomeFetch.js
+++ react-rmdb/src/hooks/useHomeFetch.js
@@ -8,33 +8,31 @@ const initialState =  {
     results: [],
     total_pages: 0,
     total_results:0
-}
+};
 
 export const useHomeFetch = () =>
 {
-    const [state, setState] = useState();
+    const [state, setState] = useState(initialState);
     const [loading, setLoading] = useState(false);
     const [error,setError] = useState(false);
 
-    const fetchMovies = async (page, searchTerm = "") => {
+    const fetchMovies = async (page, searchTerm = '') => {
         try {
-            setError(false);
-            setLoading(true);
-
-            const movies = await API.fetchMovies(searchTerm,page);
-
-            setState(prev => ({
-                ...movies,
-                results: 
-                    page > 1 ? [...prev.results, ...movies.results] : [...movies.results]
-            }))
-
-        } catch ( error) {
-            setError(true);
+          setError(false);
+          setLoading(true);
+    
+          const movies = await API.fetchMovies(searchTerm, page);
+    
+          setState(prev => ({
+            ...movies,
+            results:
+              page > 1 ? [...prev.results, ...movies.results] : [...movies.results]
+          }));
+        } catch (error) {
+          setError(true);
         }
-
         setLoading(false);
-    };
+      };
 
     // Initial render
     useEffect(() => {
