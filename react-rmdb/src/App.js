3f9d27f66717a84b216af28911f9fccda4725c7f
Hakan Tutkun <hakantutkun@gmail.com>
2021-11-27 23:33:16 +0300

    Created Home component...

--- react-rmdb/src/App.js
+++ react-rmdb/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 
 // Import Components
 import Header from './components/Header';
+import Home from './components/Home';
 
 // Import Styles
 import { GlobalStyle } from './GlobalStyle';
@@ -11,6 +12,7 @@ function App() {
   return (
     <div className="App">
       <Header/>
+      <Home/>
       <GlobalStyle />
     </div>
   );
